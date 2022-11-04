f930c2a2be17369e380273bed0e38898a0de7eb0
CYBER NETICS <cybernetics.me@outlook.com>
2022-11-04 20:32:00 +0530

    small updates

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -1,1 +1,13 @@
-hi
+@php
+$chart = {
+        type: 'bar',
+        data: {
+         labels: [2012, 2013, 2014, 2015, 2016],
+         datasets: [{
+            label: 'Raisins', data: [12, 6, 5, 18, 12]}, {
+            label: 'Bananas', data: [4, 8, 16, 5, 5]
+         }]
+        }
+};
+@endphp
+<img src="https://quickchart.io/chart?c={{ urlencode($chart) }}"/>
