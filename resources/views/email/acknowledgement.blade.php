55b6cf7e7af842f605f20fe834474b0513ac926c
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-11-25 18:36:56 +0530

    small updates

--- resources/views/email/acknowledgement.blade.php
+++ resources/views/email/acknowledgement.blade.php
@@ -13,7 +13,7 @@ So, thank you for being one of the ‘doers’, the ones willing to take positiv
 
 Now, let’s see what the results show!<br><br>
 
-<center><h3><a href="https://quiz.cybernetics.me/quiz/report/{{$qid}}/" target="_blank">Download your Report</a></h3></center><br><br>
+<center><h3><a href="http://quiz.lifestyledesignquiz.com/quiz/report/{{$qid}}/" target="_blank">Download your Report</a></h3></center><br><br>
 
 Kind regards,<br>
 Andrew
