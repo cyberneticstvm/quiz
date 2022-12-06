b8a7fdcf097078daf2b6ea644252c938b2bedc59
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-12-06 07:10:46 +0530

    small updates

--- resources/views/email/acknowledgement.blade.php
+++ resources/views/email/acknowledgement.blade.php
@@ -1,4 +1,4 @@
-Dear {{ first_name }},<br><br>
+Dear {{ $first_name }},<br><br>
 Thank you for making an investment in your future by taking the Lifestyle Design Quiz<br><br>
 The following pages are your personalised report, specifically designed to help you get a deeper understanding of yourself and clarity on how to take smart-steps right away, to clear the fog and move yourself closer toward that Maximised Life that you have dreamed about.<br><br>
 
