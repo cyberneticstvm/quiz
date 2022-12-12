d8646b65051e1ad30ee5fccfd55465db05f53c83
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-12-12 19:37:06 +0530

    small updates

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -223,7 +223,7 @@
             <p>I help people redesign their lives so they can learn, live and lead better – with passion and purpose. </p>
             <p>I want to help you too.</p><br>
             <!--<div class="text-center"><a href="https://www.blueprintlifeacademy.com.au/" target="_blank"><img src="./assets/images/signature-banner-small.png" width="450" class="img-fluid" /></a></div>-->
-            <div class="signature"><img src="./assets/images/signature.jpg" class="img-fluid" width="10%" /></div>
+            <div class="signature"><img src="./assets/images/signature.jpg" class="img-fluid" width="15%" /></div>
             <div class="call1"><a href="https://www.blueprintlifecoaching.com.au/bookings-checkout/let-s-talk-life/book" target="_blank">BOOK YOUR DISCOVERY CALL</a></div>
         </div>
     </div>
