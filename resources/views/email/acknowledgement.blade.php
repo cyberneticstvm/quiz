54a6ac88a5534b697764ff654fc7a7c0627c4a7e
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-12-26 14:55:34 +0530

    small updates

--- resources/views/email/acknowledgement.blade.php
+++ resources/views/email/acknowledgement.blade.php
@@ -1,8 +1,8 @@
 Dear {{ $first_name }},<br><br>
 Thank you for making an investment in your future by taking the Lifestyle Design Quiz<br><br>
-The following pages are your personalised report, specifically designed to help you get a deeper understanding of yourself and clarity on how to take smart-steps right away, to clear the fog and move yourself closer toward that Maximised Life that you have dreamed about.<br><br>
+The attached pages are your personalised report, specifically designed to help you get a deeper understanding of yourself and clarity on how to take smart-steps right away, to clear the fog and move yourself closer toward that Maximised Life that you have dreamed about.<br><br>
 
-The Strength Profile and the Clarity Questions will give you some insights on how you bring your best self to the table for greater impact in any lifestyle domain.<br><br>
+The Strength Profile and the Clarity Questions will give you some insights on how you bring your best self to the table for greater impact.<br><br>
 
 You can’t do everything at once, so the Focus for Growth results will show you what matters most to you right now and will offer some smart, low-input, big impact steps you can take right away to initiate positive momentum in your daily living.<br><br>
 
